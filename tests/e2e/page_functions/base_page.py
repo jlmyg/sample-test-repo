bb0131d9466bd5b2fa7908ec446f36fa0d711431
louis garcia <louis@bposeats.com>
2024-02-14 19:54:05 +0800

    e2e tests - refactored and added reporting to correct directory

--- tests/e2e/page_functions/base_page.py
+++ tests/e2e/page_functions/base_page.py
@@ -3,18 +3,33 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from tests.e2e.configurations.logger import LogGen
 
+logger = LogGen.loggen()
 
 class BasePage():
 
     def __init__(self, driver):
         self.driver = driver
 
+    def info_log(self, msg):
+        "Used to log information"
+        logger.info(msg)
+
+    def error_log(self, msg):
+        "Used to log errors"
+        logger.error(msg)
+
     def xpath_to_tuple(self, xpath):
         return (By.XPATH, xpath)
 
     def get_element(self, xpath):
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.xpath_to_tuple(xpath)))
+        try:
+            self.info_log('Finding locator {}'.format(xpath))
+            return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.xpath_to_tuple(xpath)))
+        except TimeoutException:
+            self.error_log('Locator {} does not exist'.format(xpath))
 
     def click(self, xpath):
         self.get_element(xpath).click()
--- tests/e2e/test_cases/test_case_01.py
+++ tests/e2e/test_cases/test_case_01.py
@@ -12,6 +12,7 @@ class TestNavigation(BaseTest):
         home_page = HomePageFunctions(self.driver)
 
         assert home_page.get_page_title() == "DEMOQA"
+        home_page.info_log('Test Case: test_navigation_to_demo_qa executed successfully')
 
     def test_navigate_to_elements(self):
         """ The happy flow of navigating to DemoQA Website - Elements Page"""
@@ -21,6 +22,7 @@ class TestNavigation(BaseTest):
         home_page.navigate_to_elements()
 
         assert home_page.get_current_url() == "https://demoqa.com/elements"
+        home_page.info_log('Test Case: test_navigate_to_elements executed successfully')
 
     def test_navigate_to_forms(self):
         """ The happy flow of navigating to DemoQA Website - Forms Page"""
@@ -30,6 +32,7 @@ class TestNavigation(BaseTest):
         home_page.navigate_to_forms()
 
         assert home_page.get_current_url() == "https://demoqa.com/forms"
+        home_page.info_log('Test Case: test_navigate_to_forms executed successfully')
 
     def test_navigate_to_alerts_windows(self):
         """ The happy flow of navigating to DemoQA Website - Alerts, Frame & Windows Page"""
@@ -39,6 +42,7 @@ class TestNavigation(BaseTest):
         home_page.navigate_to_alerts_frames_windows()
 
         assert home_page.get_current_url() == "https://demoqa.com/alertsWindows"
+        home_page.info_log('Test Case: test_navigate_to_alerts_windows executed successfully')
 
     def test_navigate_to_widgets(self):
         """ The happy flow of navigating to DemoQA Website - Widgets Page"""
@@ -48,6 +52,7 @@ class TestNavigation(BaseTest):
         home_page.navigate_to_widgets()
 
         assert home_page.get_current_url() == "https://demoqa.com/widgets"
+        home_page.info_log('Test Case: test_navigate_to_widgets executed successfully')
 
     def test_navigate_to_interactions(self):
         """ The happy flow of navigating to DemoQA Website - Interaction Page"""
@@ -57,3 +62,4 @@ class TestNavigation(BaseTest):
         home_page.navigate_to_interactions()
 
         assert home_page.get_current_url() == "https://demoqa.com/interaction"
+        home_page.info_log('Test Case: test_navigate_to_interactions executed successfully')
