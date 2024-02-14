63b7c578b3597b20de5c69cd93c84945cd80795a
louis garcia <louis@bposeats.com>
2024-02-14 18:50:21 +0800

    [selenium tests] - added new test cases for test navigation

--- tests/e2e/test_cases/test_case_01.py
+++ tests/e2e/test_cases/test_case_01.py
@@ -3,7 +3,7 @@ from tests.e2e.test_cases.base_test import BaseTest
 from tests.e2e.page_functions.home_page_functions import HomePageFunctions
 
 class TestNavigation(BaseTest):
-    """This class contains test for navigating to the DemoQA website"""
+    """This class contains test for navigations to the DemoQA website"""
 
     @pytest.mark.smoke
     def test_navigation_to_demo_qa(self):
@@ -21,3 +21,39 @@ class TestNavigation(BaseTest):
         home_page.navigate_to_elements()
 
         assert home_page.get_current_url() == "https://demoqa.com/elements"
+
+    def test_navigate_to_forms(self):
+        """ The happy flow of navigating to DemoQA Website - Forms Page"""
+
+        home_page = HomePageFunctions(self.driver)
+
+        home_page.navigate_to_forms()
+
+        assert home_page.get_current_url() == "https://demoqa.com/forms"
+
+    def test_navigate_to_alerts_windows(self):
+        """ The happy flow of navigating to DemoQA Website - Alerts, Frame & Windows Page"""
+
+        home_page = HomePageFunctions(self.driver)
+
+        home_page.navigate_to_alerts_frames_windows()
+
+        assert home_page.get_current_url() == "https://demoqa.com/alertsWindows"
+
+    def test_navigate_to_widgets(self):
+        """ The happy flow of navigating to DemoQA Website - Widgets Page"""
+
+        home_page = HomePageFunctions(self.driver)
+
+        home_page.navigate_to_widgets()
+
+        assert home_page.get_current_url() == "https://demoqa.com/widgets"
+
+    def test_navigate_to_interactions(self):
+        """ The happy flow of navigating to DemoQA Website - Interaction Page"""
+
+        home_page = HomePageFunctions(self.driver)
+
+        home_page.navigate_to_interactions()
+
+        assert home_page.get_current_url() == "https://demoqa.com/interaction"
