04327585abeb8b2622b6c74843105a059f02a166
louis garcia <louis@bposeats.com>
2024-02-14 18:49:57 +0800

    [selenium tests] - added browser headless capabilities v1 for chrome

--- tests/e2e/test_cases/conftest.py
+++ tests/e2e/test_cases/conftest.py
@@ -3,16 +3,25 @@
 
 import pytest
 from datetime import datetime
-from pytest_html_reporter import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from tests.e2e.configurations.config import BaseConfigs
 
+@pytest.fixture(scope='function')
+def driver_init(headless, request: pytest.FixtureRequest):
 
-@pytest.fixture(scope="function")
-def driver_init(request: pytest.FixtureRequest):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = webdriver.ChromeOptions()
+    chrome_prefs = {}
+    options.experimental_options["prefs"] = chrome_prefs
+    if headless.lower() == "true":
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--dist=loadfile")
+        options.add_argument("window-size=1920,1080")
+        options.add_argument("start-maximized")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     request.cls.driver = driver
     driver.get(BaseConfigs.url)
     driver.implicitly_wait(10)
@@ -21,10 +30,18 @@ def driver_init(request: pytest.FixtureRequest):
     collect_screenshot(driver, request)
     driver.close()
 
-
 def collect_screenshot(driver, request):
     """Method is called during teardown to collect screenshots"""
     currentdatetime = datetime.now().strftime(("%m_%d_%y_%X"))
     currentdatestring = currentdatetime.replace(":", "_")
     screenshotname = f"{request.node.originalname}_{currentdatestring}"
     driver.save_screenshot('./reports/screenshots/{}.png'.format(screenshotname))
+
+def pytest_addoption(parser: pytest.Parser):
+    """Add additional terminal params here
+    """
+    parser.addoption("--headless", action="store", default="false", help="parameter in setting up headless browser")
+
+@pytest.fixture()
+def headless(request: pytest.FixtureRequest) -> str:
+    return request.config.getoption("--headless")
