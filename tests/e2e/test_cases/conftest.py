ebc5fbff189948aa31d9a62676341f8a59c3fbaa
louis garcia <louis@bposeats.com>
2024-02-14 18:05:12 +0800

    [selenium tests] - reporting - added screenshots scripts

--- tests/e2e/test_cases/conftest.py
+++ tests/e2e/test_cases/conftest.py
@@ -1,7 +1,8 @@
 # This page contains configurations for the webdriver and the base test class.
 
-from datetime import datetime
+
 import pytest
+from datetime import datetime
 from pytest_html_reporter import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -15,14 +16,15 @@ def driver_init(request: pytest.FixtureRequest):
     request.cls.driver = driver
     driver.get(BaseConfigs.url)
     driver.implicitly_wait(10)
+    driver.maximize_window()
     yield
-    # collect_screenshot(driver, request)
+    collect_screenshot(driver, request)
     driver.close()
 
 
-# def collect_screenshot(driver, request):
-#     """Method is called during teardown to collect screenshots"""
-#     currentdatetime = datetime.now().strftime(("%m_%d_%y_%X"))
-#     currentdatestring = currentdatetime.replace(":", "_")
-#     screenshotname = f"{request.node.originalname}_{currentdatestring}"
-#     driver.save_screenshot('./reports/screenshots/{}.png'.format(screenshotname))
+def collect_screenshot(driver, request):
+    """Method is called during teardown to collect screenshots"""
+    currentdatetime = datetime.now().strftime(("%m_%d_%y_%X"))
+    currentdatestring = currentdatetime.replace(":", "_")
+    screenshotname = f"{request.node.originalname}_{currentdatestring}"
+    driver.save_screenshot('./reports/screenshots/{}.png'.format(screenshotname))
